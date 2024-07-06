8601dff0292377e41df8e9fb674208d95d30986d
rayson <157789704+RaysonHan@users.noreply.github.com>
2024-07-06 20:07:15 +0800

    Create 1002.cpp

---
+++ Basic Level/1002.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    string n;
+    cin >> n;
+
+    int digitSum = 0;
+    for(char digitChar : n) {
+        digitSum += digitChar - '0';
+    }
+
+    string sumStr = to_string(digitSum);
+
+    string pinyinMap[10] = {"ling", "yi", "er", "san", "si", "wu", "liu", "qi", "ba", "jiu"};
+
+    for (size_t i = 0; i < sumStr.length(); ++i) {
+        if (i > 0) cout << " ";
+        cout << pinyinMap[sumStr[i] - '0'];
+    }
+
+    return 0;
+}
