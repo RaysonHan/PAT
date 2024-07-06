3b032a0c404e91985b96ad3cced0866223c05f55
rayson <157789704+RaysonHan@users.noreply.github.com>
2024-07-06 19:54:46 +0800

    Add files via upload

---
+++ Basic Level/1001.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int k = 0;
+
+    while(n != 1) {
+        k++;
+        if(n % 2 == 0) {
+            n >>= 1;
+        } else {
+            n = (3 * n + 1) >> 1;
+        }
+    }
+    cout << k;
+
+    return 0;
+}
